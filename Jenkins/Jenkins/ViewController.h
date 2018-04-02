1428c37761e5f6a3aea71faff9446c1c888fafdd
lin-tong <1076860592@qq.com>
2018-04-02 23:04:38 +0800

    JenkinsTest

---
+++ Jenkins/Jenkins/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Jenkins
+//
+//  Created by 全速 on 2018/4/2.
+//  Copyright © 2018年 lin-tong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Jenkins/Jenkins/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  Jenkins
+//
+//  Created by 全速 on 2018/4/2.
+//  Copyright © 2018年 lin-tong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(100, 200, 200, 100)];
+    view.backgroundColor = [UIColor orangeColor];
+    [self.view addSubview:view];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
